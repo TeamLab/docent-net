14d1c743a80744c4489642eaf85d3fe82bc6380b
Eunbin Park <parkeb417@gmail.com>
2022-11-14 14:42:10 +0900

    DOC-11 klue/roberta-large finetuning

--- machine_reading_comprehension/main.py
+++ machine_reading_comprehension/main.py
@@ -1,39 +1,166 @@
+import argparse
+import logging
 import os
+import sys
 import yaml
 from easydict import EasyDict
 
-from klue_data_loader import KlueMRCProcessor
-from trainer import Trainer
+from trainer import QuestionAnsweringTrainer
+from transformers import (
+    AutoConfig,
+    AutoModelForQuestionAnswering,
+    AutoTokenizer,
+    DataCollatorWithPadding,
+    EvalPrediction,
+    HfArgumentParser,
+    PreTrainedTokenizerFast,
+    TrainingArguments,
+    default_data_collator,
+    set_seed,
+)
+from transformers.trainer_utils import get_last_checkpoint
 
+from data_loader import MRCLoader
+from metrics import post_processing_function, compute_metrics
 import wandb
-import logging
-from dotenv import load_dotenv
+
+logger = logging.getLogger(__name__)
+
+
+def main(args):
+    with open("config.yaml") as f:
+        saved_config = yaml.load(f, Loader=yaml.FullLoader)
+        hparams = EasyDict(saved_config)
+
+    # Setup logging
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO
+        # handlers=[logging.StreamHandler(sys.stdout)],
+    )
+
+    wandb.init(project=hparams.project_name, entity=hparams.entity_name)
+
+    # Detecting last checkpoint.
+    last_checkpoint = None
+    if os.path.isdir(hparams.output_dir):
+        last_checkpoint = get_last_checkpoint(hparams.output_dir)
+        if last_checkpoint is None and len(os.listdir(hparams.output_dir)) > 0:
+            raise ValueError(
+                f"Output directory ({hparams.output_dir}) already exists and is not empty. "
+                "Use --overwrite_output_dir to overcome."
+            )
+        elif last_checkpoint is not None and hparams.resume_from_checkpoint is None:
+            logger.info(
+                f"Checkpoint detected, resuming training at {last_checkpoint}. To avoid this behavior, change "
+                "the `--output_dir` or add `--overwrite_output_dir` to train from scratch."
+            )
+            hparams.model_name_or_path = last_checkpoint
+
+    set_seed(hparams.seed)
+
+    training_args = TrainingArguments(
+        output_dir=hparams.output_dir,
+        do_train=args.do_train,
+        do_eval=args.do_eval,
+        evaluation_strategy="epoch",
+        save_strategy="epoch",
+        per_device_train_batch_size=hparams.train_batch_size,
+        per_device_eval_batch_size=hparams.eval_batch_size,
+        learning_rate=hparams.learning_rate,
+        adam_epsilon=hparams.adam_epsilon,
+        num_train_epochs=hparams.num_train_epochs,
+        weight_decay=hparams.weight_decay,
+        logging_steps=hparams.logging_steps,
+        seed=hparams.seed,
+        fp16=hparams.fp16,
+        warmup_steps=hparams.warmup_steps,
+        max_steps=hparams.max_steps,
+        log_level="info",
+        report_to="wandb",
+        push_to_hub=True
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        hparams.model_name_or_path,
+        revision=hparams.model_revision,
+        use_fast=True,
+    )
+    model = AutoModelForQuestionAnswering.from_pretrained(
+        hparams.model_name_or_path,
+        revision=hparams.model_revision,
+        config=training_args,
+    )
+
+    data_collator = (
+        default_data_collator
+        if hparams.pad_to_max_length
+        else DataCollatorWithPadding(tokenizer, pad_to_multiple_of=8 if hparams.fp16 else None)
+    )
+
+    loader = MRCLoader(hparams)
+
+    if args.do_train:
+        train_dataset = loader.get_dataset(evaluate=False, output_examples=False)
+
+    if args.do_eval:
+        eval_examples, eval_dataset = loader.get_dataset(evaluate=True, output_examples=True)
+
+    wandb.watch(model, log="all", log_freq=100)
+    trainer = QuestionAnsweringTrainer(
+        model=model,
+        train_dataset=train_dataset if args.do_train else None,
+        eval_dataset=eval_dataset if args.do_eval else None,
+        eval_examples=eval_examples if args.do_eval else None,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        post_process_function=post_processing_function,
+        compute_metrics=compute_metrics,
+    )
+
+    # Training
+    if args.do_train:
+        checkpoint = None
+        if hparams.resume_from_checkpoint is not None:
+            checkpoint = hparams.resume_from_checkpoint
+        elif last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        metrics = train_result.metrics
+        metrics["train_samples"] = len(train_dataset)
+        wandb.log(train_result)
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+    # Evaluation
+    if args.do_eval:
+        logger.info("*** Evaluate ***")
+        metrics = trainer.evaluate()
+
+        metrics["eval_samples"] = len(eval_dataset)
+
+        trainer.log_metrics("eval", metrics)
+        trainer.save_metrics("eval", metrics)
+
+    kwargs = {"finetuned_from": hparams.model_name_or_path, "tasks": "question-answering"}
+    if hparams.dataset_name:
+        kwargs["dataset_tags"] = hparams.dataset_name
+
+    if hparams.push_to_hub:
+        trainer.push_to_hub(**kwargs)
+    else:
+        trainer.create_model_card(**kwargs)
 
 
 if __name__ == "__main__":
-    logger = logging.getLogger(__name__)
-    load_dotenv()
-    WANDB_AUTH_KEY = os.getenv("WANDB_AUTH_KEY")
-    wandb.login(key=WANDB_AUTH_KEY)
+    parser = argparse.ArgumentParser()
 
-    with open("./config.yaml") as f:
-        saved_hparams = yaml.load(f, Loader=yaml.FullLoader)
-        hparams = EasyDict(saved_hparams)["CFG"]
-    wandb.init(entity=hparams.entity_name, project=hparams.project_name, config=hparams)
+    parser.add_argument("--do_train", action="store_true", help="Whether to run training.")
+    parser.add_argument("--do_eval", action="store_true", help="Whether to run eval on the dev set.")
 
-    processor = KlueMRCProcessor()
+    args = parser.parse_args()
 
-    train_dataset = processor.get_dataset(evaluate=False, output_examples=False)
-    test_dataset, examples, features = processor.get_dataset(evaluate=True, output_examples=True)
-
-    trainer = Trainer(hparams,
-                      train_dataset=train_dataset,
-                      test_dataset=(test_dataset, examples, features)
-                      )
-
-    global_step, tr_loss = trainer.train()
-    logger.info(f" global_step = {global_step}, average loss = {tr_loss}")
-
-    trainer.evaluate()
-
-    wandb.finish()
+    main(args)
--- machine_reading_comprehension/trainer.py
+++ machine_reading_comprehension/trainer.py
@@ -1,273 +1,99 @@
-import os
-import timeit
+# coding=utf-8
+# Copyright 2020 The HuggingFace Team All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+A subclass of `Trainer` specific to Question-Answering tasks
+"""
 
-import torch
+from transformers import Trainer
+from transformers.trainer_utils import PredictionOutput
 import wandb
-from torch.optim import AdamW
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
-from transformers import (
-    AutoTokenizer,
-    AutoModelForQuestionAnswering,
-    get_linear_schedule_with_warmup,
-    set_seed)
 
-from transformers.data.metrics.squad_metrics import (
-    compute_predictions_logits,
-    squad_evaluate,
-)
+class QuestionAnsweringTrainer(Trainer):
+    def __init__(self, *args, eval_examples=None, post_process_function=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.eval_examples = eval_examples
+        self.post_process_function = post_process_function
 
-from transformers.data.processors.squad import SquadResult
-from accelerate import Accelerator
+    def evaluate(self, eval_dataset=None, eval_examples=None, ignore_keys=None, metric_key_prefix: str = "eval"):
+        eval_dataset = self.eval_dataset if eval_dataset is None else eval_dataset
+        eval_dataloader = self.get_eval_dataloader(eval_dataset)
+        eval_examples = self.eval_examples if eval_examples is None else eval_examples
 
-import logging
-from tqdm.auto import tqdm
-from typing import List
-from dataclasses import dataclass
-from metrics import BaseMetric, klue_mrc_em, mrc_f1
+        # Temporarily disable metric computation, we will do it in the loop here.
+        compute_metrics = self.compute_metrics
+        self.compute_metrics = None
+        eval_loop = self.prediction_loop if self.args.use_legacy_prediction_loop else self.evaluation_loop
+        try:
+            output = eval_loop(
+                eval_dataloader,
+                description="Evaluation",
+                # No point gathering the predictions if there are no metrics, otherwise we defer to
+                # self.args.prediction_loss_only
+                prediction_loss_only=True if compute_metrics is None else None,
+                ignore_keys=ignore_keys,
+            )
+        finally:
+            self.compute_metrics = compute_metrics
 
-logger = logging.getLogger(__name__)
-
-
-def to_list(tensor):
-    return tensor.detach().cpu().tolist()
-
-
-@dataclass
-class QAResults:
-    results: List[SquadResult]
-
-
-class Trainer(object):
-    def __init__(self,
-                 config,
-                 train_dataset=None,
-                 test_dataset=None):
-        self.config = config
-
-        self.accelerator = Accelerator(fp16=config.fp16)
-        self.device = self.accelerator.device
-        self.metrics = BaseMetric(klue_mrc_em)
-
-        self.num_train_epochs = config.num_train_epochs
-        self.train_dataset = train_dataset
-        self.test_dataset = test_dataset
-
-        self.tokenizer = AutoTokenizer.from_pretrained(config.PLM)
-        self.model = AutoModelForQuestionAnswering.from_pretrained(config.PLM)
-        self.model.to(self.device)
-
-    def train(self):
-        train_sampler = RandomSampler(self.train_dataset)
-        train_dataloader = DataLoader(self.train_dataset, sampler=train_sampler,
-                                      batch_size=self.config.train_batch_size)
-
-        if self.config.max_steps > 0:
-            t_total = self.config.max_steps
-            self.num_train_epochs = self.config.max_steps // (
-                    len(train_dataloader) // self.config.gradient_accumulation_steps) + 1
+        if self.post_process_function is not None and self.compute_metrics is not None and self.args.should_save:
+            # Only the main node write the results by default
+            eval_preds = self.post_process_function(eval_examples, eval_dataset, output.predictions)
+            metrics = self.compute_metrics(eval_preds)
+            # Prefix all keys with metric_key_prefix + '_'
+            for key in list(metrics.keys()):
+                if not key.startswith(f"{metric_key_prefix}_"):
+                    metrics[f"{metric_key_prefix}_{key}"] = metrics.pop(key)
         else:
-            t_total = len(train_dataloader) // self.config.gradient_accumulation_steps * self.num_train_epochs
+            metrics = {}
 
-        no_decay = ["bias", "LayerNorm.weight"]
-        optimizer_grouped_parameters = [
-            {
-                "params": [p for n, p in self.model.named_parameters() if not any(nd in n for nd in no_decay)],
-                "weight_decay": self.config.weight_decay,
-            },
-            {
-                "params": [p for n, p in self.model.named_parameters() if any(nd in n for nd in no_decay)],
-                "weight_decay": 0.0,
-            }
-        ]
+        if self.args.should_log:
+            # Only the main node log the results by default
+            self.log(metrics)
+        wandb.log(metrics)
 
-        optimizer = AdamW(optimizer_grouped_parameters,
-                          lr=float(self.config.learning_rate),
-                          eps=float(self.config.adam_epsilon))
-        scheduler = get_linear_schedule_with_warmup(optimizer,
-                                                    num_warmup_steps=self.config.warmup_steps,
-                                                    num_training_steps=t_total)
+        self.control = self.callback_handler.on_evaluate(self.args, self.state, self.control, metrics)
+        return metrics
 
-        self.model, optimizer, train_dataloader = self.accelerator.prepare(self.model, optimizer, train_dataloader)
+    def predict(self, predict_dataset, predict_examples, ignore_keys=None, metric_key_prefix: str = "test"):
+        predict_dataloader = self.get_test_dataloader(predict_dataset)
 
-        logger.info("***** Running training *****")
-        logger.info("  Num examples = %d", len(self.train_dataset))
-        logger.info("  Num Epochs = %d", self.num_train_epochs)
-        logger.info("  Instantaneous batch size per device = %d", self.config.train_batch_size)
-        logger.info("  Total train batch size (w. parallel, distributed & accumulation) = %d",
-                    self.config.train_batch_size * self.accelerator.num_processes * self.config.gradient_accumulation_steps)
-        logger.info("  Gradient Accumulation steps = %d", self.config.gradient_accumulation_steps)
-        logger.info("  Total optimization steps = %d", t_total)
+        # Temporarily disable metric computation, we will do it in the loop here.
+        compute_metrics = self.compute_metrics
+        self.compute_metrics = None
+        eval_loop = self.prediction_loop if self.args.use_legacy_prediction_loop else self.evaluation_loop
+        try:
+            output = eval_loop(
+                predict_dataloader,
+                description="Prediction",
+                # No point gathering the predictions if there are no metrics, otherwise we defer to
+                # self.args.prediction_loss_only
+                prediction_loss_only=True if compute_metrics is None else None,
+                ignore_keys=ignore_keys,
+            )
+        finally:
+            self.compute_metrics = compute_metrics
 
-        global_step = 1
-        epochs_trained = 0
-        steps_trained_in_current_epoch = 0
+        if self.post_process_function is None or self.compute_metrics is None:
+            return output
 
-        if os.path.exists(self.config.model_name_or_path):
-            try:
-                checkpoint_suffix = self.config.model_name_or_path.split("-")[-1].split("/")[0]
-                global_step = int(checkpoint_suffix)
-                epochs_trained = global_step // (len(train_dataloader) // self.config.gradient_accumulation_steps)
-                steps_trained_in_current_epoch = global_step % (
-                        len(train_dataloader) // self.config.gradient_accumulation_steps)
+        predictions = self.post_process_function(predict_examples, predict_dataset, output.predictions, "predict")
+        metrics = self.compute_metrics(predictions)
 
-                logger.info("  Continuing training from checkpoint, will skip to saved global_step")
-                logger.info(f"  Continuing training from epoch {epochs_trained}")
-                logger.info(f"  Continuing training from global step {global_step}")
-                logger.info(f"  Will skip the first {steps_trained_in_current_epoch} steps in the first epoch")
-            except ValueError:
-                logger.info("  Starting fine-tuning.")
+        # Prefix all keys with metric_key_prefix + '_'
+        for key in list(metrics.keys()):
+            if not key.startswith(f"{metric_key_prefix}_"):
+                metrics[f"{metric_key_prefix}_{key}"] = metrics.pop(key)
 
-        tr_loss, logging_loss = 0.0, 0.0
-        self.model.zero_grad()
-
-        train_iterator = tqdm(range(epochs_trained, int(self.num_train_epochs)), desc="Epoch")
-        set_seed(self.config.seed)
-
-        for _ in train_iterator:
-            epoch_iterator = tqdm(train_dataloader, desc="Iteration")
-            for step, batch in enumerate(epoch_iterator):
-                batch = tuple(t.to(self.device) for t in batch)
-                inputs = {
-                    "input_ids": batch[0],
-                    "attention_mask": batch[1],
-                    "token_type_ids": batch[2],
-                    "start_positions": batch[3],
-                    "end_positions": batch[4],
-                }
-                # if self.config.version_2_with_negative:
-                # check is_impossible values in dataset
-                # inputs.update({"is_impossible": batch[5]})
-
-                if steps_trained_in_current_epoch > 0:
-                    steps_trained_in_current_epoch -= 1
-                    continue
-
-                self.model.train()
-                outputs = self.model(**inputs)
-
-                loss = outputs[0]
-                if self.config.gradient_accumulation_steps > 1:
-                    loss = loss / self.config.gradient_accumulation_steps
-
-                self.accelerator.backward(loss)
-
-                tr_loss += loss.item()
-
-                if (step + 1) % self.config.gradient_accumulation_steps == 0:
-                    torch.nn.utils.clip_grad_norm_(self.model.parameters(optimizer),
-                                                   self.config.max_grad_norm)
-
-                    optimizer.step()
-                    scheduler.step()  # Update learning rate schedule
-                    self.model.zero_grad()
-
-                    wandb.log({"loss": loss})
-
-                    global_step += 1
-
-                    # Log Metrics
-                    if self.config.logging_steps > 0 and global_step % self.config.logging_steps == 0:
-                        if self.config.evaluate_during_training:
-                            logger.info("***** Running Evaluation *****")
-                            results = self.evaluate()
-                            for key, value in results.items():
-                                wandb.log({f"eval_{key}": value})
-                                logger.info(f"  {key} = {value}")
-                            # logger.info(f"evaluate/EM = {results}")
-
-                        logging_loss = tr_loss
-
-                    if self.config.save_steps > 0 and global_step % self.config.save_steps == 0:
-                        output_dir = os.path.join(self.config.output_dir, f"checkpoint-{global_step}")
-                        if not os.path.exists(output_dir):
-                            os.makedirs(output_dir)
-                        model_to_save = self.model.module if hasattr(self.model, "module") else self.model
-                        model_to_save.save_pretrained(output_dir)
-                        self.tokenizer.save_pretrained(output_dir)
-
-                        logger.info(f"Saving model checkpoint to {output_dir}")
-
-                        torch.save(optimizer.state_dict(), os.path.join(output_dir, "optimizer.pt"))
-                        torch.save(scheduler.state_dict(), os.path.join(output_dir, "scheduler.pt"))
-                        logger.info(f"Saving optimizer and scheduler states to {output_dir}")
-
-                if 0 < self.config.max_steps < global_step:
-                    epoch_iterator.close()
-                    break
-            if 0 < self.config.max_steps < global_step:
-                train_iterator.close()
-                break
-
-        return global_step, tr_loss / global_step
-
-    def evaluate(self):
-        dataset, examples, features = self.test_dataset
-        eval_sampler = SequentialSampler(dataset)
-        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=self.config.eval_batch_size)
-
-        logger.info("***** Running evaluation *****")
-        logger.info(f"  Num examples = {len(dataset)}")
-        logger.info(f"  Batch size = {self.config.eval_batch_size}")
-
-        all_result = []
-        start_time = timeit.default_timer()
-
-        for batch in tqdm(eval_dataloader, desc="Evaluation"):
-            self.model.eval()
-
-            with torch.no_grad():
-                batch = tuple(t.to(self.device) for t in batch)
-                inputs = {
-                    "input_ids": batch[0],
-                    "attention_mask": batch[1],
-                    "token_type_ids": batch[2],
-                }
-                outputs = self.model(**inputs)
-                start_logits = outputs.start_logits
-                end_logits = outputs.end_logits
-
-            feature_indices = batch[3].tolist()
-
-            for i, feature_index in enumerate(feature_indices):
-                unique_id = int(features[feature_index].unique_id)
-                single_example_start_logits = to_list(start_logits[i])
-                single_example_end_logits = to_list(end_logits[i])
-
-                result = SquadResult(unique_id, single_example_start_logits, single_example_end_logits)
-                all_result.append(result)
-
-        eval_time = timeit.default_timer() - start_time
-        logger.info(
-            f"  Evaluation done in total {eval_time} secs {eval_time / len(self.test_dataset)} sec per example)")
-
-        output_prediction_file = os.path.join(self.config.output_dir, "predictions.json")
-        output_nbest_file = os.path.join(self.config.output_dir, "nbest_predictions.json")
-
-        if self.config.version_2_with_negative:
-            output_null_log_odds_file = os.path.join(self.config.output_dir, "null_odds.json")
-        else:
-            output_null_log_odds_file = None
-
-        predictions = compute_predictions_logits(
-            all_examples=examples,
-            all_features=features,
-            all_results=all_result,
-            n_best_size=self.config.n_best_size,
-            max_answer_length=self.config.max_answer_length,
-            do_lower_case=self.config.do_lower_case,
-            tokenizer=self.tokenizer,
-            output_null_log_odds_file=output_null_log_odds_file,
-            null_score_diff_threshold=self.config.null_score_diff_threshold,
-            version_2_with_negative=self.config.version_2_with_negative,
-            verbose_logging=self.config.verbose_logging,
-            output_prediction_file=output_prediction_file,
-            output_nbest_file=output_nbest_file,
-        )
-
-        # em_result = self.metrics(predictions, examples)
-        test_result = mrc_f1(predictions, examples)
-        # logger.info("***** EM results *****")
-        # logger.info(f"  EM = {em_result}")
-        # wandb.log({"EM": em_result})
-
-        return test_result
+        return PredictionOutput(predictions=predictions.predictions, label_ids=predictions.label_ids, metrics=metrics)
